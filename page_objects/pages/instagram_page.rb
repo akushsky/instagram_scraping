53c358bc185a8fa128e9c85b3dcba3f7fbde244f
akushsky <akushsky@it-universe.ru>
2015-10-13 23:39:43 +0400

    Add new login page and button

--- page_objects/pages/instagram_page.rb
+++ page_objects/pages/instagram_page.rb
@@ -1,11 +1,14 @@
 module PageObjects
   module Pages
     class InstagramPage < SitePrism::Page
-      set_url Settings.instagram_root_url
+      set_url Settings.instagram_login_url
 
       element :username, '#lfFieldInputUsername'
       element :password, '#lfFieldInputPassword'
-      element :login,    "button[data-reactid='.0.1.0.1.0.1.0.1.3'"
+      element :login,    "button[data-reactid='.0.0.1.0.1.3'"
+
+      ### Id of old button on main page
+      #'.0.1.0.1.0.1.0.1.3'"
 
       element :search_input, "input[data-reactid='.0.2.0.1.$searchBox.0']"
     end
