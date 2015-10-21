bbc044e215f101d08168997b84884add579ee990
akushsky <akushsky@it-universe.ru>
2015-10-21 22:34:57 +0400

    Revert

--- page_objects/pages/instagram_page.rb
+++ page_objects/pages/instagram_page.rb
@@ -3,9 +3,12 @@ module PageObjects
     class InstagramPage < SitePrism::Page
       set_url Settings.instagram_login_url
 
-      element :username, "[name='username']"
-      element :password, "[name='password']"
-      element :login,    "button[data-reactid='.0.1.0.1.0.1.0.2']"
+      element :username, '#lfFieldInputUsername'
+      element :password, '#lfFieldInputPassword'
+      element :login,    "button[data-reactid='.0.0.1.0.1.3'"
+
+      ### Id of old button on main page
+      #'.0.1.0.1.0.1.0.1.3'"
 
       element :search_input, "input[data-reactid='.0.2.0.1.$searchBox.0']"
     end
