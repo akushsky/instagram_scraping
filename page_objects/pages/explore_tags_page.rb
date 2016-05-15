8333ff3eb63fdbe818353fc532e851436d4d2d12
akushsky <akushsky@it-universe.ru>
2016-05-15 14:09:28 +0300

    Fix tags page a class

--- page_objects/pages/explore_tags_page.rb
+++ page_objects/pages/explore_tags_page.rb
@@ -3,7 +3,7 @@ module PageObjects
     class ExploreTagsPage < SitePrism::Page
       set_url "#{Settings.instagram_root_url}/explore/tags{/hash_tag}/"
 
-      elements :posts, "a[class='_8mlbc _t5r8b']"
+      elements :posts, "a[class='_8mlbc _vbtk2 _t5r8b']"
 #'a.-cx-PRIVATE-PostsGridItem__root.-cx-PRIVATE-PostsGrid__item'
       elements :likes,  'span.whiteoutSpriteHeartOpen'
       elements :comment_forms, 'form.-cx-PRIVATE-PostInfo__commentCreator'
